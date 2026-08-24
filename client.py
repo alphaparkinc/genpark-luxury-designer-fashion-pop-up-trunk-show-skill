3e3d66c030b8b4809bd644a5d252fe43f7d36066
GenPark <135203240+alphaparkinc@users.noreply.github.com>
2026-08-24 09:41:11 +0800

    feat: add client.py

---
+++ client.py
@@ -0,0 +1,11 @@
+class LuxuryDesignerFashionPopUpTrunkShowClient:
+    def curate_vip_couture_trunk_show(self, designer_label='Tarun Tahiliani Bridal Couture', vip_guest_tier='HNW_GLOBAL_DIASPORA'):
+        return {
+            'trunk_show_id': 'psl_trk_8812',
+            'designer_label': designer_label,
+            'curated_couture_pieces_count': 35,
+            'average_ticket_value_inr': 380000.0,
+            'virtual_stylist_3d_draping_available': True,
+            'global_private_trunk_show_cities': ['London Mayfair', 'New York Manhattan', 'Dubai Downtown'],
+            'bespoke_made_to_order_cleared': True
+        }
